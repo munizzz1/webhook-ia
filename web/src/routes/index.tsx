d5ca690994b7b4fb1d574daeacc897df4be353cf
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2025-11-03 19:23:59 -0300

    feat: frontend config

---
+++ web/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { Sidebar } from "../components/ui/sidebar";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="h-screen bg-zinc-900">
+      <PanelGroup direction="horizontal">
+        <Panel defaultSize={28} minSize={15} maxSize={40}>
+          <Sidebar />
+        </Panel>
+
+        <PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150" />
+
+        <Panel defaultSize={88} minSize={68}>
+          conteudo
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
+}
