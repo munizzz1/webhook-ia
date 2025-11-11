91cbc8d853ebf62ebd25f1f40b2c52fb64e4f151
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2025-11-11 17:42:03 -0300

    feat: create crud api webhook

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,12 +3,15 @@ import { fastifySwagger } from "@fastify/swagger";
 import { fastifyCors } from "@fastify/cors";
 import { fastify } from "fastify";
 import {
-	serializerCompiler,
-	validatorCompiler,
-	jsonSchemaTransform,
-	type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
+import { deleteWebhook } from "./routes/delete-webhook";
+import { listWebhooks } from "./routes/list-webhooks";
+import { getWebhook } from "./routes/get-webhook";
 import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
@@ -17,26 +20,30 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
-	origin: true,
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-	// credentials: true,
+  origin: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  // credentials: true,
 });
 
 app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "Webhook Inspector API",
-			description: "API for capturing and inspecting webhook requests",
-			version: "1.0.0",
-		},
-	},
-	transform: jsonSchemaTransform,
+  openapi: {
+    info: {
+      title: "Webhook Inspector API",
+      description: "API for capturing and inspecting webhook requests",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
 });
 
 app.register(ScalarApiReference, {
-	routePrefix: "/docs",
+  routePrefix: "/docs",
 });
 
+app.register(getWebhook);
+app.register(deleteWebhook);
+app.register(listWebhooks);
+
 app.listen({ port: env.PORT, host: "0.0.0.0" }).then(() => {
-	console.log("HTTP server running!");
+  console.log("HTTP server running!");
 });
